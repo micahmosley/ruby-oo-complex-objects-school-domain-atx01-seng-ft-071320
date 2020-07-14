7cad721ce687ae9ece606ebd9a62a18f143695b3
Micah Mosley <micah.mosley@utexas.edu>
2020-07-14 20:27:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,12 @@ class School
   
   def add_student (name, grade)
     if @roster[grade]==nil
-    @roster[grade]
+      @roster[grade]=[]
+      @roster[grade].push(name) 
+    else 
+      @roster[grade].push(name)
+    end 
+  end 
+  
   
 end 
