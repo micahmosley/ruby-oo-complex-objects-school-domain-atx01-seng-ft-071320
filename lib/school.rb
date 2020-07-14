c2fbbbcdbee0490c71b9c5bd9a6038aafb6dd545
Micah Mosley <micah.mosley@utexas.edu>
2020-07-14 20:37:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School 
+  def initialize (name)
+    @name=name 
+    @roster={}
+  end 
+  
+  def roster
+    @roster
+  end 
+  def add_student (name, grade)
+    if @roster[grade]==nil
+      @roster[grade]=[]
+      @roster[grade].push(name) 
+    else 
+      @roster[grade].push(name)
+    end 
+  end 
+  
+  def grade (num)
+    @roster[num]
+  end 
+  
+  def sort 
+    @roster.each do |grade, names|
+      @roster[grade]=names.sort 
+    end 
+    @roster
+      
+  end 
+end 
